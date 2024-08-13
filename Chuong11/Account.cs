7f9e96ffb6b70b1369a00d7a63cca1229530eb6f
PhanTu2551993 <phanvantu25051993@gmail.com>
2024-08-13 16:30:31 +0700

    Add project files.

---
+++ Chuong11/Account.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chuong11
+{
+    public class Account
+    {
+        public int AccountNumber { get; set; }
+        public string AccountName { get; set; }
+        public double AccountBalance { get; set; }
+
+        public Account(int accountNumber, string accountName, double accountBalance)
+        {
+            AccountNumber = accountNumber;
+            AccountName = accountName;
+            AccountBalance = accountBalance;
+        }
+
+        public void Deposit(double amount)
+        {
+            AccountBalance += amount;
+        }
+
+        public void Withdraw(double amount)
+        {
+            if (AccountBalance >= amount)
+            {
+                AccountBalance -= amount;
+            }
+            else
+            {
+                Console.WriteLine("Số tiền trong tài khoản còn không đủ !");
+            }
+
+        }
+    }
+}
